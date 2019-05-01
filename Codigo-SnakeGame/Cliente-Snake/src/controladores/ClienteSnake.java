94d7874f46049fc6c3f524234ecb91c0e925d9f6
LuisAuana <luis.bonilla.escuela@hotmail.com>
2019-05-01 17:32:59 -0500

    Creación interfaz gráfica más validación del nombre

--- Codigo-SnakeGame/Cliente-Snake/src/controladores/ClienteSnake.java
+++ Codigo-SnakeGame/Cliente-Snake/src/controladores/ClienteSnake.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controladores;
 
 import javafx.application.Application;
@@ -12,8 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
- * @author luisb
+ * Esta clase se encarga de inicializar la interfaz gráfica.
+ * @author Luis Aduana.
  */
 public class ClienteSnake extends Application {
     
--- Codigo-SnakeGame/Cliente-Snake/src/controladores/VistaPrincipalController.java
+++ Codigo-SnakeGame/Cliente-Snake/src/controladores/VistaPrincipalController.java
@@ -1,30 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
- *
- * @author luisb
+ * Clase que gestiona los eventos en la VistaPrincipal.
+ * @author Luis Aduana.
  */
 public class VistaPrincipalController implements Initializable {
+       
+    @FXML
+    TextField nombreJugador;
+    @FXML
+    Label mensajeError;
+    
+    private String validarNickname() {
+        if ("".equals(nombreJugador.getText().trim())) {
+            return "Ingrese un nombre";
+        } else if (nombreJugador.getText().trim().length() > 12) {
+            return "Nombre debe ser menor a 12 caracteres";
+        } else if (nombreJugador.getText().trim().length() < 3) {
+            return "Nombre es demasiado corto";
+        } else {
+            
+            Pattern patron = Pattern.compile("^([a-z]|[A-Z]|[0-9]){3,12}$");
+            Matcher match = patron.matcher(nombreJugador.getText().trim());
+            if (match.find()) {
+                return "";
+            } else 
+                return "El nombre solo puede contener minúsculas, mayúsculas y/o números";
+        }
+    }
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void iniciarJuego(ActionEvent event) {
+        String validacion = validarNickname();
+        if ("".equals(validacion)) {
+            mensajeError.setText(validacion);
+            System.out.println("ENTRO");
+            // TODO Se inicia el juego aquí <-----------------------------------------
+        } else {
+            mensajeError.setText(validacion);
+            /*Alert dialogo = new Alert(AlertType.INFORMATION);
+            dialogo.setTitle("Nombre incorrecto");
+            dialogo.setHeaderText(null);
+            dialogo.setContentText(validacion);
+            dialogo.initStyle(StageStyle.UTILITY);
+            dialogo.showAndWait();*/
+        }
     }
     
     @Override
