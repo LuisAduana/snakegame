dccf08333a7453acf3fcc22fbe08cf77e03b3f49
rodrigo <rodrigo.op1791@gmail.com>
2019-06-05 22:03:29 -0500

    Commit post-revisión

---
+++ Codigo-SnakeGame/Cliente-Snake/src/JPA/Puntuacion.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JPA;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ferzo
+ */
+@Entity
+@Table(name = "PUNTUACION")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Puntuacion.findAll", query = "SELECT p FROM Puntuacion p")
+    , @NamedQuery(name = "Puntuacion.findByIdcliente", query = "SELECT p FROM Puntuacion p WHERE p.idcliente = :idcliente")
+    , @NamedQuery(name = "Puntuacion.findByNombre", query = "SELECT p FROM Puntuacion p WHERE p.nombre = :nombre")
+    , @NamedQuery(name = "Puntuacion.findByPuntuacion", query = "SELECT p FROM Puntuacion p WHERE p.puntuacion = :puntuacion")})
+public class Puntuacion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "IDCLIENTE")
+    private Integer idcliente;
+    @Basic(optional = false)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Column(name = "PUNTUACION")
+    private Integer puntuacion;
+
+    public Puntuacion() {
+    }
+
+    public Puntuacion(Integer idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public Puntuacion(Integer idcliente, String nombre) {
+        this.idcliente = idcliente;
+        this.nombre = nombre;
+    }
+
+    public Integer getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(Integer idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(Integer puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idcliente != null ? idcliente.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Puntuacion)) {
+            return false;
+        }
+        Puntuacion other = (Puntuacion) object;
+        if ((this.idcliente == null && other.idcliente != null) || (this.idcliente != null && !this.idcliente.equals(other.idcliente))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "DAO.Puntuacion[ idcliente=" + idcliente + " ]";
+    }
+    
+}
--- Codigo-SnakeGame/Cliente-Snake/src/controladores/TablaPuntuacionesController.java
+++ Codigo-SnakeGame/Cliente-Snake/src/controladores/TablaPuntuacionesController.java
@@ -5,13 +5,21 @@
  */
 package controladores;
 
+import JPA.Puntuacion;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -31,12 +39,26 @@ public class TablaPuntuacionesController implements Initializable {
     @FXML
     private TableColumn<?, ?> tbcPuntuacion;
 
+    ObservableList<Puntuacion> puntuaciones;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Cliente-SnakePU");
+        EntityManager em = emf.createEntityManager();
+   
+        em.getTransaction().begin();
+        int x=0;
+        while(em.find(Puntuacion.class, x)!= null){
+            Puntuacion puntuacion = em.find(Puntuacion.class, x);
+            puntuaciones.add(puntuacion);
+            x++;
+        }
+        
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
     }    
     
 }
