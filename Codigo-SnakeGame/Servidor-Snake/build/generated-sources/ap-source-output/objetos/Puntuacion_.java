bde155716b36daa0665654f020ff9df1c8b18b78
Luis Bonilla <luis.bonilla.escuela@hotmail.com>
2019-06-10 17:15:06 -0500

    Se registran las puntuaciones a la BD

--- Codigo-SnakeGame/Servidor-Snake/build/generated-sources/ap-source-output/objetos/Puntuacion_.java
+++ Codigo-SnakeGame/Servidor-Snake/build/generated-sources/ap-source-output/objetos/Puntuacion_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-10T13:33:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-10T17:08:11")
 @StaticMetamodel(Puntuacion.class)
 public class Puntuacion_ { 
 
--- Codigo-SnakeGame/Servidor-Snake/src/servidor/snake/ActualizarSerpientes.java
+++ Codigo-SnakeGame/Servidor-Snake/src/servidor/snake/ActualizarSerpientes.java
@@ -1,15 +1,19 @@
 package servidor.snake;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import servidor.ConexionBD;
 import servidor.ServidorSnake;
+import snake.Snake;
 /**
  *
  * @author Fernando
  */
-public class ActualizarSerpientes implements Runnable{
+public class ActualizarSerpientes implements Runnable {
 
     private final ServidorSnake server;
+    private List<Snake> listaPuntuaciones;
     
     public ActualizarSerpientes(ServidorSnake server) {
         this.server = server;
@@ -20,9 +24,10 @@ public class ActualizarSerpientes implements Runnable{
         long limiteCiclo = 0;
         while (true) {
             System.out.flush(); // NOSONAR");
-            if (!this.server.getTablero().getSnakes().isEmpty()) {
-                this.server.getTablero().calcularChoques();
-                this.server.getTablero().actualizarPosicion();
+            if (!server.getTablero().getSnakes().isEmpty()) {
+                listaPuntuaciones = server.getTablero().calcularChoques();
+                registrarPuntuaciones();
+                server.getTablero().actualizarPosicion();
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException ex) {
@@ -34,6 +39,13 @@ public class ActualizarSerpientes implements Runnable{
             if (limiteCiclo == Integer.MIN_VALUE) {
                 break;
             }
+        }
+    }
+
+    private void registrarPuntuaciones() {
+        if (!listaPuntuaciones.isEmpty()) {
+            ConexionBD conexion = new ConexionBD();
+            listaPuntuaciones = conexion.registrarPuntuaciones(listaPuntuaciones);
         }
     }
     
